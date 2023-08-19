06061b9d6c71584497d59ffad589f704e42066ec
Exedice <except.dice@gmail.com>
2023-08-19 20:46:02 +0300

    added zoom and the plane translation

--- src/lib/components/RenderPanel.svelte
+++ src/lib/components/RenderPanel.svelte
@@ -5,8 +5,13 @@
 
     import * as GW from "$lib/scripts/space"
 
+    type DragData = { startMousePos: { x: number, y: number }, startWorldPos: { x: number, y: number } };
+
     let self: HTMLElement;
     let canvas: HTMLCanvasElement;
+
+    let zoom: number = 1;
+    let dragData: DragData;
 
     function init(): void {
         if (canvas == undefined) {
@@ -28,12 +33,75 @@
 
     }
 
+    // TODO: zoom with easing
+    // TODO: zoom label
+    function mouseWheel(e: WheelEvent) {
+        // console.log(e.deltaY);
+        zoom = Math.min(Math.max(zoom - e.deltaY / 1000, GW.MIN_ZOOM), GW.MAX_ZOOM);
+        GW.setZoom(zoom);
+    }
+
+    function mouseDown(e: MouseEvent) {
+        if (e.button === 0) {
+            
+            
+        } else if (e.button === 2) {
+            
+            e.preventDefault();
+            startDrag(e);
+
+        }
+    }
+
+    function startDrag(e: MouseEvent) {
+        const camPos = GW.getCameraPos();
+        dragData = {
+            startMousePos: {
+                x: e.clientX,
+                y: e.clientY
+            },
+            startWorldPos: {
+                x: camPos.x,
+                y: camPos.y
+            }
+        };
+
+        document.addEventListener('mousemove', drag);
+        document.addEventListener('mouseup', endDrag);
+        document.body.style.cursor = 'move';
+        
+        drag(e);
+    }
+    
+    // TODO: add "center" button
+    function drag(e: MouseEvent) {
+        const offsetMouse = {
+            x: e.clientX - dragData.startMousePos.x,
+            y: e.clientY - dragData.startMousePos.y
+        };
+
+        const newWorldPos = {
+            x: -(dragData.startWorldPos.x + offsetMouse.x / zoom),
+            y: dragData.startWorldPos.y + offsetMouse.y / zoom
+        };
+
+        GW.setCameraPos(newWorldPos);
+    }
+    
+    function endDrag(e: MouseEvent) {
+        document.removeEventListener('mousemove', drag);
+        document.removeEventListener('mouseup', endDrag);
+        document.body.style.cursor = 'initial';
+    }
+
     onMount(init);
 
 </script>
 
 <main
     bind:this={self}
+    on:mousedown={mouseDown}
+    on:mousewheel={mouseWheel}
 >
 
     <canvas bind:this={canvas}></canvas>
--- src/lib/scripts/space.ts
+++ src/lib/scripts/space.ts
@@ -9,6 +9,12 @@ let renderer: THREE.WebGLRenderer;
 
 let plane: Plane;
 
+// ISSUE: the range of the value is not restricted
+let zoomCoefficient: number = 1;
+
+export const MIN_ZOOM = 0.1;
+export const MAX_ZOOM = 3;
+
 function sizeIntoOrthCameraBounds(width: number, height: number): [number, number, number, number] {
     return [width / -2, width / 2, height / 2, height / -2];
 }
@@ -44,8 +50,32 @@ export function resize(width: number, height: number): void {
         return;
     }
 
-    [camera.left, camera.right, camera.top, camera.bottom] = sizeIntoOrthCameraBounds(width, height);
+    // The size needs to be divided by the zoom coefficient, not multiplied.
+    // The logic is that the zoom coefficient's value should reflect the zoom percentage.
+    // e.g. 1.5 = 150%
+    // The zoom percentage is inversely proportional to the camera size.
+    // (obvious?)
+    [camera.left, camera.right, camera.top, camera.bottom] = sizeIntoOrthCameraBounds(width / zoomCoefficient, height / zoomCoefficient);
     camera.updateProjectionMatrix();
     renderer.setSize(width, height);
 
+}
+
+export function setZoom(c: number): void {
+    zoomCoefficient = Math.min(Math.max(c, MIN_ZOOM), MAX_ZOOM);
+    resize(canvas.width, canvas.height);
+}
+
+export function setCameraPos(pos: { x: number, y: number }): void {
+    camera.position.setX(pos.x);
+    camera.position.setY(pos.y);
+}
+
+export function getCameraPos(): THREE.Vector3 {
+    if (camera === undefined) {
+        console.error("The space is not initialized. Try calling `initSpace` first.");
+        return new THREE.Vector3(); // TODO: figure something out, exception?
+    }
+
+    return camera.position;
 }
